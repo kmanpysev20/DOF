a8c0fde37f1ef383c8d129ef8253f3ddef7d171b
kmanpysev20 <kmanpysev20@gmail.com>
2023-04-10 10:43:34 +0900

    v0.01

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 
 from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://kmanpysev20:test@cluster0.cknzfkt.mongodb.net/test")
-db = client.dof
+db = client.DOFBorder
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route("/borderlist", methods=["GET"])
+def borderlist():
+    contents = list(db.Border.find({}, {'_id':False}))
+    return jsonify({"result": contents})
+
+@app.route('/post', methods=["POST"])
+def post_todo():
+    title_receive = request.form["title_give"]
+    writer_receive = request.form["writer_give"]
+    content_receive = request.form["content_give"]
+    password_receive = request.form["password_give"]
+    ymd_receive = request.form["ymd_give"]
+    doc = {
+        'title' : title_receive,
+        'writer' : writer_receive,
+        'content' : content_receive,
+        'password' : password_receive,
+        'ymd' : ymd_receive
+    }
+    db.Border.insert_one(doc)
+    return jsonify({"msg": "등록 완료!"})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- static/js/common.js
+++ static/js/common.js
@@ -1,10 +1,66 @@
+//데이터 저장
+function writeBorder() {
+    let title = $('.input-tit').val();
+    let writer = $('.input-name').val();
+    let content = $('.input-cont').val();
+    let password = $('.input-password').val();
+    let ymd = $('.ymd').text();
+
+    let formData = new FormData()
+    formData.append("title_give", title)
+    formData.append("writer_give", writer)
+    formData.append("content_give", content)
+    formData.append("password_give", password)
+    formData.append("ymd_give", ymd)
+
+    fetch('/post', { method: "POST", body: formData }).then(res => res.json()).then(data => {
+        alert(data["msg"])
+        $(location).attr("href", "../subpage/border.html")
+        // window.location.reload()
+    })
+}
+
+//글쓰기 조건 코드
+function isRegist() {
+    //제목란에 값이 없다면 팝업 노출 시키고 return false라 아래 작성자명 if문으로 가지 않음
+    //반대로, 제목란에 값이 있다면 참이니깐 > 아래 작성자명 if문이 실행
+    if ($.trim($('.input-tit').val()) == '') {
+        alert("제목 입력해주세요")
+        return false;
+    }
+    if ($.trim($('.input-name').val()) == '') {
+        alert("작성자명 입력해주세요")
+        return false;
+    }
+    if ($.trim($('.input-cont').val()) == '') {
+        alert("내용 입력해주세요")
+        return false;
+    }
+    if ($.trim($('.input-password').val()) == '') {
+        alert("비밀번호 입력해주세요")
+        return false;
+    }
+    //이렇게 참일 때 쭉가서 return true를 만나면 함수 종료
+    return true;
+}
+
+//날짜
+$(function ymd() {
+    let today = new Date();   
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+
+    $('.ymd').text(`${year}-${month}-${date}`)
+});
+
 // gnb menu click event 
 $(function () {
-    $('.btn-gnb').on('click', function() {
+    $('.btn-gnb').on('click', function () {
         $('.main-header').toggleClass('is-gnb');
         $('#gnb').toggleClass('is-gnb');
     });
-    $('.ic-gnb-close').on('click', function() {
+    $('.ic-gnb-close').on('click', function () {
         $('.main-header').removeClass('is-gnb');
         $('#gnb').removeClass('is-gnb');
     });
