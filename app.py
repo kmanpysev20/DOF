ccc41b29ebd486d5c9290f7d59318fd1206154aa
kmanpysev20 <kmanpysev20@gmail.com>
2023-05-30 11:08:26 +0900

    v0.07

--- app.py
+++ app.py
@@ -1,18 +1,134 @@
-import math
-from flask import Flask, render_template, request, jsonify, send_file
-
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for, flash
 from bson.objectid import ObjectId
-
 from pymongo import MongoClient
+from datetime import datetime
+import math, time, os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 client = MongoClient("mongodb+srv://kmanpysev20:test@cluster0.cknzfkt.mongodb.net/test")
 db = client.DOFBorder
 
-app = Flask(__name__)
+@app.template_filter("formatdatetime")
+def format_datetime(value):
+    if value is None:
+        return ""
+    
+    now_timestemp = time.time()
+    offset = datetime.fromtimestamp(now_timestemp) - datetime.utcfromtimestamp(now_timestemp)
+    value = datetime.fromtimestamp(int(value) / 1000) + offset
+    return value.strftime('%Y-%m-%d')
 
+# 시작 시 index.html 불러오기
 @app.route('/')
 def home():
     return render_template("index.html")
+
+# intro page
+@app.route('/intro')
+def intro():
+    return render_template("subpage/intro.html")
+
+# member page
+@app.route('/member')
+def member():
+    return render_template("subpage/member.html")
+
+# service page
+@app.route('/service')
+def service():
+    return render_template("subpage/service.html")
+
+# boarder-list.html 소스
+@app.route('/boarder-list')
+def boarder_list():
+
+    # page의 값 (값이 없는 경우 기본값은 1)
+    page = request.args.get('page', 1 , type=int)
+
+    # 한 페이지당 몇 개의 게시물을 출력하는 코드
+    limit = request.args.get('limit', 5 , type=int)
+
+    board = db.Boarder
+    datas = board.find({}).skip((page-1) * limit).limit(limit)
+
+    # 게시물의 총 갯수
+    tot_count = board.count_documents({})
+
+    # 마지막 페이지의 수를 구합니다.
+    last_page_num = math.ceil(tot_count / limit)
+
+    # 페이지 블록 5개씩 표기
+    block_size = 5
+
+    # 현재 블럭의 위치
+    block_num = int((page - 1) / block_size)
+
+    # 블럭의 시작 위치
+    block_start = int((block_size * block_num) + 1)
+
+    # 블럭의 끝 위치
+    block_last = math.ceil(block_start + (block_size - 1))
+
+    return render_template(
+        "subpage/boarder-list.html", 
+        datas = list(datas),
+        limit = limit,
+        page = page,
+        block_start = block_start, 
+        block_last = block_last,
+        last_page_num = last_page_num
+        )
+
+# border-write.html 소스
+@app.route("/boarder-write", methods=['GET', 'POST'])
+def board_write():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        name = request.form.get('name')
+        contents = request.form.get('contents')
+        password = request.form.get('password')
+
+        # 날짜 및 시간 구하기 소스
+        current_utc_time = round(datetime.utcnow().timestamp() * 1000)
+
+        board = db.Boarder
+        post = {
+            "title" : title,
+            "name" : name,
+            "contents" : contents,
+            "password" : password,
+            "pubdate" : current_utc_time,
+            "view" : 0,
+        }
+        x = board.insert_one(post)
+        return redirect(url_for("boarder_list", idx=x.inserted_id))
+    else:
+        return render_template("subpage/boarder-write.html")
+    
+# border-modify.html 소스
+@app.route("/boarder_modify/<idx>")
+def board_modify(idx):
+    # idx = request.args.get("idx")
+    if idx is not None:
+        page = request.args.get("page")
+
+        board = db.Boarder
+        data = board.find_one({"_id":ObjectId(idx)})
+
+        if data is not None:
+            result = {
+                "id": data.get("_id"),
+                "title": data.get("title"),
+                "name": data.get("name"),
+                "contents": data.get("contents"),
+                "password" : data.get("password"),
+                "pubdate" : data.get("pubdate"),
+                "view": data.get("view"),
+            }
+            return render_template("subpage/boarder-modify.html", result=result , page=page)
+    return abort(404)
 
 # 삭제하는 코드
 @app.route('/delete', methods=["POST"])
@@ -22,65 +138,8 @@ def delete():
     db.Border.delete_one({'_id':id_receive})
     return jsonify({"msg": "삭제 완료!"})
     
-# 수정하는 코드
-@app.route('/modify', methods=["POST"])
-def modify():
-    id_receive = request.form["id_give"]
-    id_receive = ObjectId(id_receive)
-
-    title_receive = request.form["title_give"]
-    content_receive = request.form["content_give"]
-    db.Border.update_one({'_id' : id_receive}, {'$set':{'title' : title_receive}})
-    db.Border.update_one({'_id' : id_receive}, {'$set':{'content' : content_receive}})
-    return jsonify({"msg": "수정 완료!"})
-
-
-# 각 리스트 ID 값 저장하는 코드
-# @app.route("/borderlist", methods=["GET"])
-# def borderlist():
-#     contents = list(db.Border.find({}))
-#     for content in contents:
-#         content['_id'] = str(content['_id'])
-#     return jsonify({"result": contents})
-
-@app.route("/boardlist", methods=['GET'])
-def boardlist():
-    page = request.args.get('page', 1, type=int)
-    limit = 5
-    contents = db.Border.find({}).skip((page - 1)*limit).limit(limit)
-	
-    tot_count = db.Border.find({}).count()
-    last_page_num = math.ceil(tot_count / limit)
-    
-    lists = []
-    for content in contents:
-        content['_id'] = str(content['_id'])
-        lists.append(content)
-
-    return jsonify({'list': lists,
-                    'limit': limit,
-                    'page': page,
-                    'result': contents,
-                    'last_page_num': last_page_num})
-
-# 데이터 저장하기 코드
-@app.route('/post', methods=["POST"])
-def post():
-    title_receive = request.form["title_give"]
-    writer_receive = request.form["writer_give"]
-    content_receive = request.form["content_give"]
-    password_receive = request.form["password_give"]
-    ymd_receive = request.form["ymd_give"]
-    doc = {
-        'title' : title_receive,
-        'writer' : writer_receive,
-        'content' : content_receive,
-        'password' : password_receive,
-        'ymd' : ymd_receive
-    }
-    db.Border.insert_one(doc)
-    return jsonify({"msg": "등록 완료!"})
-
 if __name__ == "__main__":
-    # app.run(debug=True, port=5000)
-    app.run()
+    app.run(debug=True, port=5000)
+
+    app.secret_key = "****"
+    app.debug = True
--- static/js/common.js
+++ static/js/common.js
@@ -1,163 +1,193 @@
-//글쓰기 조건 코드
-function isRegist() {
-    //제목란에 값이 없다면 팝업 노출 시키고 return false라 아래 작성자명 if문으로 가지 않음
-    //반대로, 제목란에 값이 있다면 참이니깐 > 아래 작성자명 if문이 실행
-    if ($.trim($('.input-tit').val()) == '') {
-        alert("제목 입력해주세요")
-        return false;
-    }
-    if ($.trim($('.input-name').val()) == '') {
-        alert("작성자명 입력해주세요")
-        return false;
-    }
-    if ($.trim($('.input-cont').val()) == '') {
-        alert("내용 입력해주세요")
-        return false;
-    }
-    if ($.trim($('.input-password').val()) == '') {
-        alert("비밀번호 입력해주세요")
-        return false;
-    }
-    //이렇게 참일 때 쭉가서 return true를 만나면 함수 종료
-    return true;
+// 게시글 비밀번호 체크
+function boarderClick() {
+    $('.table-wrap').on('click', '.list', function () {
+        const row = $(this).find('td');
+        let password = row.eq(4).text()
+        console.log(password);
+        listClick(password)
+    });
 }
 
-//border 등록
-function borderlist() {
-    fetch("/borderlist").then(res => res.json()).then(data => {
-        let rows = data['result'];
-        // $('.list').empty();
-        rows.forEach((row, index) => {
-            let idSum = index + 1;
-            let title = row['title'];
-            let writer = row['writer'];
-            let content = row['content'];
-            let password = row['password'];
-            let ymd = row['ymd'];
-            let _id = row['_id'];
-            let temp_html = `<tr class="list">
-                            <td>${idSum}</td>
-                            <td>${title}</td>
-                            <td>${writer}</td>
-                            <td style="display:none;">${content}</td>
-                            <td style="display:none;">${password}</td>
-                            <td>${ymd}</td>
-                            <td style="display:none;">${_id}</td>
-                            </tr>`
-            $('.table-wrap').append(temp_html);
-        })
-        $('.table-wrap').on('click', '.list', function () {
-            const row = $(this).find('td');
-            listClick(row)
-        });
+function listClick(password) {
+    $('.title').on('click', function () {
+        let prom = prompt("비밀번호 입력")
+        console.log(prom)
+
+        if (prom === password) {
+            $(location).attr("href",
+                `../subpage/border-modify.html?
+        &password=${encodeURIComponent(password)}`)
+        } else {
+            alert("취소")
+        }
     })
 }
 
-//border 게시물 클릭
-function listClick(row) {
-    let ymd = row.eq(0).text()
-    let title = row.eq(1).text()
-    let writer = row.eq(2).text()
-    let content = row.eq(3).text()
-    let password = row.eq(4).text()
-    let _id = row.eq(6).text()
-    console.log(password)
-
-    let promPasss = prompt('비밀번호룰 입력하세요', '숫자만 입력');
-    let passNum = Number(promPasss);
-    console.log(passNum);
-
-    if(passNum === password) {
-        $(location).attr("href",
-        `../subpage/border-modify.html?
-    ymd=${encodeURIComponent(ymd)}
-    &title=${encodeURIComponent(title)}
-    &writer=${encodeURIComponent(writer)}
-    &content=${encodeURIComponent(content)}
-    &password=${encodeURIComponent(password)}
-    &_id=${encodeURIComponent(_id)}`)
-    } else {
-        alert("실행불가")
+//글쓰기 유효성 코드
+function Checkform() {
+    if (form.title.value == "") {
+        form.title.focus();
+        alert("제목을 입력해주세요")
+        return false;
+    }
+    if (form.name.value == "") {
+        form.name.focus();
+        alert("이름을 입력해주세요")
+        return false;
+    }
+    if (form.contents.value == "") {
+        form.contents.focus();
+        alert("내용을 입력해주세요")
+        return false;
+    }
+    if (form.password.value == "") {
+        form.password.focus();
+        alert("비밀번호를 입력해주세요")
+        return false;
     }
 }
 
-//글등록 데이터 저장
-function writeBorder() {
-    let title = $('.input-tit').val();
-    let writer = $('.input-name').val();
-    let content = $('.input-cont').val();
-    let password = $('.input-password').val();
-    let ymd = $('.ymd').text();
+// //border 등록
+// function borderlist() {
+//     fetch("/borderlist").then(res => res.json()).then(data => {
+//         let rows = data['result'];
+//         // $('.list').empty();
+//         rows.forEach((row, index) => {
+//             let idSum = index + 1;
+//             let title = row['title'];
+//             let writer = row['writer'];
+//             let content = row['content'];
+//             let password = row['password'];
+//             let ymd = row['ymd'];
+//             let _id = row['_id'];
+//             let page = row['page'];
 
-    let formData = new FormData()
-    formData.append("title_give", title)
-    formData.append("writer_give", writer)
-    formData.append("content_give", content)
-    formData.append("password_give", password)
-    formData.append("ymd_give", ymd)
+//             let temp_html = `<tr class="list">
+//                             <td>${idSum}</td>
+//                             <td>${title}</td>
+//                             <td>${writer}</td>
+//                             <td style="display:none;">${content}</td>
+//                             <td style="display:none;">${password}</td>
+//                             <td>${ymd}</td>
+//                             <td style="display:none;">${_id}</td>
+//                             </tr>`
+//             $('.table-wrap').append(temp_html);
 
-    fetch('/post', { method: "POST", body: formData }).then(res => res.json()).then(data => {
-        alert(data["msg"])
-        $(location).attr("href", "../subpage/border.html")
-        // window.location.reload()
-    })
-}
+//         })
+//         $('.table-wrap').on('click', '.list', function () {
+//             const row = $(this).find('td');
+//             listClick(row)
+//         });
+//     })
+// }
 
-function modifylist() {
-    const queryString = window.location.search;
-    const searchParams = new URLSearchParams(decodeURIComponent(queryString));
-    console.log(searchParams);
-    let _id = searchParams.get("_id");
-    let title = searchParams.get("title");
-    let writer = searchParams.get("writer");
-    let content = searchParams.get("content");
-    let password = searchParams.get("password");
-    console.log("_id : " + _id);
-    console.log("타이틀: " + title);
-    console.log("작성자 : " + writer);
-    console.log("내용 : " + content);
-    console.log("비밀번호 : " + password);
-    $(".board-read-wrap").attr("id", _id);
-    $(".input-tit").val(title).attr("id", _id);
-    $(".input-name").val(writer).attr("id", _id);
-    $(".input-cont").val(content).attr("id", _id);
-    $(".input-password").val(password).attr("id", _id);
-  
-    return _id;
-  }
+// //border 게시물 클릭
+// function listClick(row) {
+//     let ymd = row.eq(0).text()
+//     let title = row.eq(1).text()
+//     let writer = row.eq(2).text()
+//     let content = row.eq(3).text()
+//     let password = row.eq(4).text()
+//     let _id = row.eq(6).text()
+//     console.log(password)
 
-function modifyBorder(_id) {
-    let title = $(".input-tit").val();
-    let content = $(".input-cont").val();
-  
-    console.log("_id : " + _id);
-    console.log("타이틀: " + title);
-    console.log("작성자 : " + content);
-  
-    let formData = new FormData();
-    formData.append("id_give", _id);
-    formData.append("title_give", title);
-    formData.append("content_give", content);
-  
-    fetch("/modify", { method: "POST", body: formData })
-      .then((res) => res.json())
-      .then((data) => {
-        alert(data["msg"]);
-        $(location).attr("href", "../subpage/border.html");
-      });
-  }
+//     let promPasss = prompt('비밀번호룰 입력하세요', '숫자만 입력');
+//     console.log(promPasss);
+
+//     if(promPasss === password) {
+//         $(location).attr("href",
+//         `../subpage/border-modify.html?
+//     ymd=${encodeURIComponent(ymd)}
+//     &title=${encodeURIComponent(title)}
+//     &writer=${encodeURIComponent(writer)}
+//     &content=${encodeURIComponent(content)}
+//     &password=${encodeURIComponent(password)}
+//     &_id=${encodeURIComponent(_id)}`)
+//     } else {
+//         alert("비밀번호가 틀리거나 취소를 하였습니다.")
+//     }
+// }
+
+// //글등록 데이터 저장
+// function writeBorder() {
+//     let title = $('.input-tit').val();
+//     let writer = $('.input-name').val();
+//     let content = $('.input-cont').val();
+//     let password = $('.input-password').val();
+//     let ymd = $('.ymd').text();
+
+//     let formData = new FormData()
+//     formData.append("title_give", title)
+//     formData.append("writer_give", writer)
+//     formData.append("content_give", content)
+//     formData.append("password_give", password)
+//     formData.append("ymd_give", ymd)
+
+//     fetch('/post', { method: "POST", body: formData }).then(res => res.json()).then(data => {
+//         alert(data["msg"])
+//         $(location).attr("href", "../subpage/border.html")
+//         // window.location.reload()
+//     })
+// }
+
+// // 수정 코드
+// function modifylist() {
+//     const queryString = window.location.search;
+//     const searchParams = new URLSearchParams(decodeURIComponent(queryString));
+//     console.log(searchParams);
+//     let _id = searchParams.get("_id");
+//     let title = searchParams.get("title");
+//     let writer = searchParams.get("writer");
+//     let content = searchParams.get("content");
+//     let password = searchParams.get("password");
+//     console.log("_id : " + _id);
+//     console.log("타이틀: " + title);
+//     console.log("작성자 : " + writer);
+//     console.log("내용 : " + content);
+//     console.log("비밀번호 : " + password);
+//     $(".board-read-wrap").attr("id", _id);
+//     $(".input-tit").val(title).attr("id", _id);
+//     $(".input-name").val(writer).attr("id", _id);
+//     $(".input-cont").val(content).attr("id", _id);
+//     $(".input-password").val(password).attr("id", _id);
+
+//     return _id;
+//   }
+
+// function modifyBorder(_id) {
+//     let title = $(".input-tit").val();
+//     let content = $(".input-cont").val();
+
+//     console.log("_id : " + _id);
+//     console.log("타이틀: " + title);
+//     console.log("작성자 : " + content);
+
+//     let formData = new FormData();
+//     formData.append("id_give", _id);
+//     formData.append("title_give", title);
+//     formData.append("content_give", content);
+
+//     fetch("/modify", { method: "POST", body: formData })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         alert(data["msg"]);
+//         $(location).attr("href", "../subpage/border.html");
+//       });
+//   }
+
+
 //글수정 삭제
 function deleteBorder(_id) {
     let formData = new FormData()
     formData.append("id_give", _id)
 
     fetch('/delete', { method: "POST", body: formData })
-    .then((res) => res.json())
-    .then((data) => {
-        alert(data["msg"]);
-        $(location).attr("href", "../subpage/border.html")
-        // window.location.reload()
-    })
+        .then((res) => res.json())
+        .then((data) => {
+            alert(data["msg"]);
+            $(location).attr("href", "../subpage/border.html")
+            // window.location.reload()
+        })
 }
 
 
@@ -165,7 +195,7 @@ function deleteBorder(_id) {
 function cancle() {
     //YES
     if (confirm("정말 취소할까요?")) {
-        $(location).attr("href", "../subpage/border.html")
+        $(location).attr("pathname", "boarder-list")
         //NO
     } else {
     }
@@ -214,14 +244,6 @@ $(function () {
     });
 });
 
-
-//취소 했을 때
-function cancle() {
-    if (confirm("정말 취소할까요?")) {
-        $(location).attr("href", "../subpage/border.html")
-    } else {
-    }
-}
 
 //popupCancle
 function popupCancle() {
